9292baf6f58c59b77f409e3ac3d1f94d721c990f
Quinn Gao <gaoqz.cs@gmail.com>
2025-09-21 20:05:21 -0700

    fix: When pub doesn't show up in some wikis

--- main.js
+++ main.js
@@ -283,12 +283,10 @@
          * @returns {boolean} Whether publisher matches
          */
         matchPublisher: function (coins, rule) {
-            const coinsPub = coins['rft.pub'] || coins['rft.jtitle'];
-            if (!coinsPub || !rule['pub']) return false;
-
-            // Also consider author fields as potential publisher names
-            const coinsAuthor = coins['rft.au'] || coins['rft.aulast'];
+            const coinsPub = coins['rft.pub'] || coins['rft.jtitle'] || [];
+            const coinsAuthor = coins['rft.au'] || coins['rft.aulast'];  // Also consider author fields as potential publisher names
             if (coinsAuthor) coinsPub.push(...CiteUnseen.ensureArray(coinsAuthor));
+            if (!coinsPub || !rule['pub']) return false;
 
             if (!rule._cachedPublisherRegex) {
                 rule._cachedPublisherRegex = new RegExp(rule['pub'], 'i');
