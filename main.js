d9a94e0ff2f7b74c6b2e490f0c70b6512078620b
KevinPayravi <kevinpayravi@gmail.com>
2025-10-13 03:50:34 -0400

    Add div.refbegin>ul as reflist

--- main.js
+++ main.js
@@ -1548,7 +1548,7 @@
 
             // Find all reflists and track citations within each
             const reflists = Array.from(
-                document.querySelectorAll('#mw-content-text .mw-parser-output ol.references'),
+                document.querySelectorAll('#mw-content-text .mw-parser-output ol.references, #mw-content-text .mw-parser-output div.refbegin>ul'),
                 (reflist) => reflist.parentNode
             );
             CiteUnseen.reflists = [];
